12fe2edc9dbab06d473c7378ced9b0b48d3fc653
Misho <nnamper@gmail.com>
2023-04-01 19:01:58 +0400

    Moving ball

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 #[derive(Component)]
 struct Name(String);
 
 #[derive(Component)]
 struct Person;
+
+#[derive(Component)]
+struct Ball;
+
+#[derive(Component)]
+struct Position {
+    vec3: Vec3,
+}
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Ran d".to_string())));
@@ -24,10 +32,56 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_startup_system(add_people)
             .add_system(greet_people);
+    }
+}
+
+pub struct BallPlugin;
+
+impl Plugin for BallPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_systems((add_ball, add_camera, draw_ball))
+            .add_system(ball_movement);
+    }
+}
+
+fn add_ball(mut commands: Commands) {
+    commands.spawn((
+        Ball,
+        Position {
+            vec3: Vec3::new(-150.0, 0.0, 0.0),
+        },
+    ));
+}
+
+fn add_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn draw_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    query: Query<&Position, With<Ball>>,
+) {
+    for pos in &query {
+        commands.spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::PURPLE)),
+                transform: Transform::from_translation(pos.vec3),
+                ..default()
+            },
+            Ball,
+        ));
+    }
+}
+
+fn ball_movement(mut ball_position: Query<(&mut Ball, &mut Transform)>) {
+    for (_ball, mut transform) in &mut ball_position {
+        transform.translation.x += 1.;
     }
 }
 
@@ -37,6 +91,6 @@ struct GreetTimer(Timer);
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_plugin(BallPlugin)
         .run();
 }
