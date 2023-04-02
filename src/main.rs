7b25b52c3bcfc1d6f95b351fa1db0ccc98c854f2
Misho <nnamper@gmail.com>
2023-04-02 21:43:27 +0400

    Movement and simple out of bounds detection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::PresentMode};
+
+const BOUNDS: Vec2 = Vec2::new(800.0, 640.0);
 
 #[derive(Component)]
 struct Name(String);
@@ -6,82 +8,54 @@ struct Name(String);
 #[derive(Component)]
 struct Person;
 
+enum Direction {
+    WEST,
+    EAST,
+}
+
 #[derive(Component)]
-struct Ball;
-
-#[derive(Component)]
-struct Position {
-    vec3: Vec3,
+struct Ball {
+    direction: Direction,
 }
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Ran d".to_string())));
-    commands.spawn((Person, Name("York d".to_string())));
-    commands.spawn((Person, Name("Shel k".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("Hello {}!", name.0)
-        }
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
-}
-
 pub struct BallPlugin;
 
 impl Plugin for BallPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_systems((add_ball, add_camera, draw_ball))
-            .add_system(ball_movement);
+        app.add_startup_system(setup_ball).add_system(ball_movement);
     }
 }
 
-fn add_ball(mut commands: Commands) {
+fn setup_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
     commands.spawn((
-        Ball,
-        Position {
-            vec3: Vec3::new(-150.0, 0.0, 0.0),
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_xyz(0. - BOUNDS.x, 0., 0.),
+            ..default()
+        },
+        Ball {
+            direction: Direction::EAST,
         },
     ));
 }
 
-fn add_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
-}
-
-fn draw_ball(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    query: Query<&Position, With<Ball>>,
-) {
-    for pos in &query {
-        commands.spawn((
-            MaterialMesh2dBundle {
-                mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-                material: materials.add(ColorMaterial::from(Color::PURPLE)),
-                transform: Transform::from_translation(pos.vec3),
-                ..default()
-            },
-            Ball,
-        ));
-    }
-}
-
 fn ball_movement(mut ball_position: Query<(&mut Ball, &mut Transform)>) {
-    for (_ball, mut transform) in &mut ball_position {
-        transform.translation.x += 1.;
+    for (mut ball, mut transform) in &mut ball_position {
+        if transform.translation.x >= BOUNDS.x {
+            ball.direction = Direction::WEST;
+        } else if transform.translation.x <= -BOUNDS.x {
+            ball.direction = Direction::EAST;
+        }
+
+        match ball.direction {
+            Direction::WEST => transform.translation.x -= 1.,
+            Direction::EAST => transform.translation.x += 1.,
+        }
     }
 }
 
@@ -90,7 +64,16 @@ struct GreetTimer(Timer);
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "I am a window!".into(),
+                resolution: (BOUNDS.x, BOUNDS.y).into(),
+                present_mode: PresentMode::AutoVsync,
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugin(BallPlugin)
         .run();
 }
